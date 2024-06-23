861f26e63397eeee4f6a49269faa2713c5dd149d
Emmanuel Akalazu <138693982+Enetreal@users.noreply.github.com>
2024-06-23 16:12:16 +0100

    0x03-log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,36 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+'''a script that reads stdin line by line and computes metrics'''
 
 import sys
 
-def print_stats(sum_file_size, status_code):
-    """Prints the accumulated metrics"""
-    print('File size: {:d}'.format(sum_file_size))
-    for key in sorted(status_code.keys()):
-        if status_code[key] != 0:
-            print('{}: {}'.format(key, status_code[key]))
-
-i = 0
-sum_file_size = 0
-status_code = {'200': 0,
-               '301': 0,
-               '400': 0,
-               '401': 0,
-               '403': 0,
-               '404': 0,
-               '405': 0,
-               '500': 0}
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
 try:
     for line in sys.stdin:
-        args = line.split()
-        if len(args) > 6:
-            status_line = args[-2]
-            file_size = args[-1]
-            if status_line in status_code:
-                status_code[status_line] += 1
-            try:
-                sum_file_size += int(file_size)
-            except ValueError:
-                pass
-            i += 1
-            if i == 10:
-                print_stats(sum_file_size, status_code)
-                i = 0
-except KeyboardInterrupt:
-    print_stats(sum_file_size, status_code)
-    raise
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
 finally:
-    print_stats(sum_file_size, status_code)
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
