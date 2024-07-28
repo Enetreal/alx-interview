69c0cde276dd7fb7205a8ee9a8dae9d520cc00c4
Emmanuel Akalazu <138693982+Enetreal@users.noreply.github.com>
2024-07-28 13:34:34 +0100

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,23 @@
+#!/usr/bin/python3
+"""A function to determine the fewest number of coins needed
+   to meet a given amount total"""
+
+
 def makeChange(coins, total):
+    """This function will take a list of coins and use
+       that to calculate how much change the total will require
+    """
     if total <= 0:
         return 0
 
     else:
-        coins.sort(reverse=True)
+        coin = sorted(coins)
+        coin.reverse()
         counter = 0
-        for coin in coins:
-            while total >= coin:
+        for i in coin:
+            while(total >= i):
                 counter += 1
-                total -= coin
+                total -= i
         if total == 0:
             return counter
         return -1
