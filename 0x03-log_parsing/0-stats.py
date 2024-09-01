119934c3e0f4e07b6521a11bc13472cbc97570d1
Emmanuel Akalazu <138693982+Enetreal@users.noreply.github.com>
2024-09-01 21:08:59 +0100

    0x03-log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,46 @@
 #!/usr/bin/python3
-"""A script that reads stdin line by line and computes metrics."""
+
+""" script that reads stdin line by line and computes metrics """
 
 import sys
-from collections import defaultdict
 
-total_file_size = 0
-status_codes = defaultdict(int)
+
+def printStatus(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
+
+
+# sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
-    for i, line in enumerate(sys.stdin, 1):
-        try:
-            ip_address, date, request, status_code, file_size = line.strip().split(" - ", 1)[0].split(" - ")
-            status_code = int(status_code.split(" ")[1])
-            file_size = int(file_size)
-            status_codes[status_code] += 1
-            total_file_size += file_size
-        except (ValueError, IndexError):
-            continue
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printStatus(statusCodes, size)
 
-        if i % 10 == 0:
-            print(f"File size: {total_file_size}")
-            for code in sorted(status_codes):
-                if status_codes[code] > 0:
-                    print(f"{code}: {status_codes[code]}")
-            print()
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except Exception:
+            pass
+
+        try:
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
+        except Exception:
+            pass
+    printStatus(statusCodes, size)
+
 
 except KeyboardInterrupt:
-    print(f"File size: {total_file_size}")
-    for code in sorted(status_codes):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+    printStatus(statusCodes, size)
+    raise
